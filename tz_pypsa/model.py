01e18995ba6485e4392f84316e8354ebb1cdb0b9
Aman Majid <39566844+amanmajid@users.noreply.github.com>
2024-08-30 15:26:16 +0100

    add simple example model

--- tz_pypsa/model.py
+++ tz_pypsa/model.py
@@ -349,3 +349,182 @@ class Model:
                 model_name,
             ) 
         )
+    
+    @staticmethod
+    def load_example_model(
+        year : int = 2030,
+        add_emissions_constraint : bool = True,
+    ):
+        '''
+        Creates a simple model for testing and demos. This code is adapted from Fabian Neumann's PyPSA tutorial: https://fneum.github.io/data-science-for-esm/09-workshop-pypsa.html
+
+        Parameters
+        ----------
+
+            path_to_dir : str
+                Directory from which we load the model. This directory should contain at least one yaml file and a data/ directory containing the timeseries and cost data. File names are strictly enforced.
+
+        Returns
+        ----------
+
+            network : pypsa.Network.
+                A PyPSA network object representing the loaded model.
+
+        Raises
+        ----------
+
+            ValueError: If the specified model is not found in the stock models.
+
+        Example
+        ----------
+        
+            You can call the function as follows:
+
+            >>> model = Model.load_example_model()
+        
+        '''
+
+        url = f"https://raw.githubusercontent.com/PyPSA/technology-data/master/outputs/costs_{year}.csv"
+        costs = pd.read_csv(url, index_col=[0, 1])
+
+
+        costs.loc[costs.unit.str.contains("/kW"), "value"] *= 1e3
+        costs.unit = costs.unit.str.replace("/kW", "/MW")
+
+        defaults = {
+            "FOM": 0,
+            "VOM": 0,
+            "efficiency": 1,
+            "fuel": 0,
+            "investment": 0,
+            "lifetime": 25,
+            "CO2 intensity": 0,
+            "discount rate": 0.07,
+        }
+        costs = costs.value.unstack().fillna(defaults)
+
+        costs.at["OCGT", "fuel"] = costs.at["gas", "fuel"]
+        costs.at["CCGT", "fuel"] = costs.at["gas", "fuel"]
+        costs.at["OCGT", "CO2 intensity"] = costs.at["gas", "CO2 intensity"]
+        costs.at["CCGT", "CO2 intensity"] = costs.at["gas", "CO2 intensity"]
+
+        def annuity(r, n):
+            return r / (1.0 - 1.0 / (1.0 + r) ** n)
+
+        annuity(0.07, 20)
+
+        costs["marginal_cost"] = costs["VOM"] + costs["fuel"] / costs["efficiency"]
+
+        annuity = costs.apply(lambda x: annuity(x["discount rate"], x["lifetime"]), axis=1)
+
+        costs["capital_cost"] = (annuity + costs["FOM"] / 100) * costs["investment"]
+
+        url = (
+            "https://tubcloud.tu-berlin.de/s/pKttFadrbTKSJKF/download/time-series-lecture-2.csv"
+        )
+
+        ts = pd.read_csv(url, index_col=0, parse_dates=True)
+
+        ts.load *= 1e3
+
+        resolution = 4
+        ts = ts.resample(f"{resolution}h").first()
+
+        n = pypsa.Network()
+
+        n.add("Bus", "electricity")
+
+        n.set_snapshots(ts.index)
+
+        n.snapshot_weightings.loc[:, :] = resolution
+
+        carriers = [
+            "onwind",
+            "offwind",
+            "solar",
+            "OCGT",
+            "hydrogen storage underground",
+            "battery storage",
+        ]
+
+        n.madd(
+            "Carrier",
+            carriers,
+            color=["dodgerblue", "aquamarine", "gold", "indianred", "magenta", "yellowgreen"],
+            co2_emissions=[costs.at[c, "CO2 intensity"] for c in carriers],
+        )
+
+        n.add(
+            "Load",
+            "demand",
+            bus="electricity",
+            p_set=ts.load,
+        )
+
+        n.add(
+            "Generator",
+            "OCGT",
+            bus="electricity",
+            carrier="OCGT",
+            capital_cost=costs.at["OCGT", "capital_cost"],
+            marginal_cost=costs.at["OCGT", "marginal_cost"],
+            efficiency=costs.at["OCGT", "efficiency"],
+            p_nom_extendable=True,
+        )
+
+        for tech in ["onwind", "offwind", "solar"]:
+            n.add(
+                "Generator",
+                tech,
+                bus="electricity",
+                carrier=tech,
+                p_max_pu=ts[tech],
+                capital_cost=costs.at[tech, "capital_cost"],
+                marginal_cost=costs.at[tech, "marginal_cost"],
+                efficiency=costs.at[tech, "efficiency"],
+                p_nom_extendable=True,
+            )
+
+        n.add(
+            "StorageUnit",
+            "battery storage",
+            bus="electricity",
+            carrier="battery storage",
+            max_hours=6,
+            capital_cost=costs.at["battery inverter", "capital_cost"]
+            + 6 * costs.at["battery storage", "capital_cost"],
+            efficiency_store=costs.at["battery inverter", "efficiency"],
+            efficiency_dispatch=costs.at["battery inverter", "efficiency"],
+            p_nom_extendable=True,
+            cyclic_state_of_charge=True,
+        )
+
+        capital_costs = (
+            costs.at["electrolysis", "capital_cost"]
+            + costs.at["fuel cell", "capital_cost"]
+            + 168 * costs.at["hydrogen storage underground", "capital_cost"]
+        )
+
+        n.add(
+            "StorageUnit",
+            "hydrogen storage underground",
+            bus="electricity",
+            carrier="hydrogen storage underground",
+            max_hours=168,
+            capital_cost=capital_costs,
+            efficiency_store=costs.at["electrolysis", "efficiency"],
+            efficiency_dispatch=costs.at["fuel cell", "efficiency"],
+            p_nom_extendable=True,
+            cyclic_state_of_charge=True,
+        )
+
+        if add_emissions_constraint:
+            n.add(
+                "GlobalConstraint",
+                "CO2Limit",
+                carrier_attribute="co2_emissions",
+                sense="<=",
+                constant=0,
+            )
+
+        return n
