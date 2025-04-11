bdccd900aa6ab04e8fcecf17d116154c8ef197a3
Irfan <irfanm@transitionzero.org>
2025-04-11 13:24:07 +0000

    Changed 'year' to 'years' in load_csv_from_dir for consistency

--- tz_pypsa/model.py
+++ tz_pypsa/model.py
@@ -342,7 +342,7 @@ class Model:
     @staticmethod
     def load_csv_from_dir(
         path_to_dir,
-        year,
+        years,
         backstop : bool = False,
         **kwargs,
     ) -> pypsa.Network:
@@ -355,7 +355,7 @@ class Model:
 
             path_to_dir : str
                 Directory from which we load the model. This directory should contain csv files. File names are strictly enforced.
-            year: int or list
+            years: int or list
                 The year(s) to load in the model, which can be supplied as an integer or a list of integers.
             backstop : bool (optional)
                 If True, the model will include backstop technologies (default is False).
@@ -395,10 +395,10 @@ class Model:
                 )
         
         # --- choose years to load in --- #
-        if isinstance(year, list):
-            network.snapshots = network.snapshots[network.snapshots.year.isin(year)]
+        if isinstance(years, list):
+            network.snapshots = network.snapshots[network.snapshots.year.isin(years)]
         else:
-            network.snapshots = network.snapshots[network.snapshots.year == year]
+            network.snapshots = network.snapshots[network.snapshots.year == years]
 
         return network
 
