d3016244bc10f0785c6f84bd2ccc53d26965464c
Joel <joelyap1120@hotmail.com>
2025-05-05 13:13:06 +0100

    Error wraper on bus long name mapping to deal with region where long name wasnt inputted

--- tz_pypsa/wrangle.py
+++ tz_pypsa/wrangle.py
@@ -462,14 +462,18 @@ def transform_visualiser_hourly_output(
     )
 
     # --- Map bus long names to the DataFrame ---
-    buses_long_name = network.buses.long_name
-    merged_df = merged_df.merge(
-        buses_long_name, 
-        how='left', 
-        left_on='Node', 
-        right_index=True
-    )
-
+    try:    
+        buses_long_name = network.buses.long_name
+        merged_df = merged_df.merge(
+            buses_long_name, 
+            how='left', 
+            left_on='Node', 
+            right_index=True
+        )
+    
+    except Exception as e:
+        print(f"Bus long names not found: {e}")
+        
     return merged_df
 
 def transform_visualiser_yearly_output(
